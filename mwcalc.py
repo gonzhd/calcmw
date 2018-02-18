1ac425f4a4bbfe69aa412dc7bc6bd6233c355d28
Gonzalo Hernandez <gonzalo@vismagnetica.com>
2018-02-18 00:34:58 -0300

    title added

--- mwcalc.py
+++ mwcalc.py
@@ -1,9 +1,22 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Calculation of the Moleular Weight with corresponding uncertainty 
+for a given Molecular Formula. 
+The molecular weight is calculated by summing over the atomic weights of the 
+constituents elements. 
 
-This is a temporary script file.
+    MW = Sum(N_i*atw_i)
+    
+The atomic weights, in turn, are calculated as the weighted average of the atomic
+masses using the isotopic distribution as weights.
+
+    atw_i = Sum(w_j*atm_j)
+    
+Uncertainties are calculated by uncertainty propagation of these two equations.
+
+>-----
+  Gonzalo Hernandez - Vis Magnetica - Feb 2018
+                                           -----<
 """
 
 import json
